e36a6e4711489ebaf45bd2930d2d3bc936187a3a
Juber Ahmed <a.juber@hotmail.com>
2018-03-19 18:47:02 -0400

    Added Store to App

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        App is loading...
-      </div>
+      <Provider store={store}>
+        <div className="App">App is loading...</div>
+      </Provider>
     );
   }
 }
